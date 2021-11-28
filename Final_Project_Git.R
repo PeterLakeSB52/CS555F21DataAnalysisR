85a7619f0f6c2044789b081ba59e109c30e29c28
Peter Lake <70727287+PeterLakeSB52@users.noreply.github.com>
2021-11-27 21:49:54 -0500

    Desktop save 11/27

--- Final_Project_Git.R
+++ Final_Project_Git.R
@@ -11,6 +11,7 @@ lat = as.vector(data$Latitude)
 BULon = as.vector(rep(-71.10640804519835, times=239))
 BULat = as.vector(rep(42.350252194515846, times=239))
 distance = distHaversine(cbind(lon, lat), cbind(BULon, BULat))
+#Converting meters to miles and saving to the dataframe
 data$distance = round((distance*0.000621371), 2)
 
 #Distribution of response variable
@@ -21,11 +22,22 @@ boxplot(data$BU_2020_Apps)
 hist(log(data$BU_2020_Apps))
 data$BU_Log_Apps = log(data$BU_2020_Apps)
 
+#Scatterplot matrix
+pairs(data[,10:16], pch = 19, col = "dodgerblue")
+
+
 #Visualizing relationship between various predictors and the response variable
-plot(data$BU_2020_Apps, data$X2020.Applied.Count)
+plot(data$UGDS, data$BU_Log_Apps)
+plot(data$X2020.Applied.Count, data$BU_Log_Apps)
+plot(data$X2020.Matriculant.Count, data$BU_Log_Apps)
+plot(data$distance, data$BU_Log_Apps)
+plot(data$LSATMedDiff, data$BU_Log_Apps)
+plot(data$GPAMedDiff, data$BU_Log_Apps)
+
+
 
 #Multiple Linear Regression
-model = lm(data$BU_2020_Apps ~ data$UGDS + data$X2020.Applied.Count + data$LSATMedDiff + data$GPAMedDiff)
+model = lm(data$BU_Log_Apps ~ data$UGDS + data$X2020.Applied.Count + data$LSATMedDiff + data$GPAMedDiff)
 summary(model)
-plot(data$BU_2020_Apps, model$residuals)
-hist(log(data$BU_2020_Apps))
+plot(data$BU_Log_Apps, model$residuals)
+abline(a = 0, b = 0, col = "red")
